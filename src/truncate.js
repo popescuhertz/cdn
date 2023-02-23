37200f3e0cf600c0fdaf982bee3f063bcc7797db
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-23 15:10:43 -0800

    Simplified the code

--- src/truncate.js
+++ src/truncate.js
@@ -1,132 +1,83 @@
-const debounce = (func, delay) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
-
-const lineHeightCache = new Map();
-
-const getLineHeight = (element) => {
-  const cacheKey = element.tagName + element.className;
-  if (lineHeightCache.has(cacheKey)) {
-    return lineHeightCache.get(cacheKey);
-  }
-  const lineHeight = parseFloat(getComputedStyle(element).lineHeight);
-  lineHeightCache.set(cacheKey, lineHeight);
-  return lineHeight;
-};
-
-const truncateText = (element) => {
-  const container = element.querySelector("p, h2, h3, h4, h5, h6, div, span");
-  if (!container) return;
-
-  const text = container.innerHTML;
-  let maxLines;
-
-  if (window.innerWidth < 768) {
-    maxLines =
-      parseInt(element.getAttribute("data-max-lines-mobile")) ||
-      parseInt(element.getAttribute("data-max-lines")) ||
-      Infinity;
-  } else if (window.innerWidth < 1024) {
-    maxLines =
-      parseInt(element.getAttribute("data-max-lines-tablet")) ||
-      parseInt(element.getAttribute("data-max-lines")) ||
-      Infinity;
-  } else {
-    maxLines =
-      parseInt(element.getAttribute("data-max-lines-desktop")) ||
-      parseInt(element.getAttribute("data-max-lines")) ||
-      Infinity;
-  }
-
-  const lineHeight = getLineHeight(container);
-  const maxContainerHeight = lineHeight * maxLines;
-
-  container.style.height = "auto";
-  const fullContainerHeight = container.offsetHeight;
-  container.style.height = "";
-
-  if (fullContainerHeight > maxContainerHeight) {
-    let truncatedText = text;
-    while (
-      container.offsetHeight > maxContainerHeight &&
-      truncatedText.length > 0
-    ) {
-      truncatedText = truncatedText.slice(0, -1);
-      container.innerHTML = truncatedText + "...";
-    }
-
-    const showMoreBtn = element.querySelector(".show-more");
-    const showLessBtn = element.querySelector(".show-less");
-    const shouldShowButtons = container.offsetHeight < fullContainerHeight;
-
-    if (showMoreBtn && showLessBtn) {
-      showMoreBtn.style.display = shouldShowButtons ? "inline" : "none";
-      showLessBtn.style.display = "none";
-
-      showMoreBtn.addEventListener("click", () => {
-        container.innerHTML = text;
-        showMoreBtn.style.display = "none";
-        showLessBtn.style.display = "inline";
-      });
-
-      showLessBtn.addEventListener("click", () => {
-        container.innerHTML = truncatedText + "...";
-        showMoreBtn.style.display = "inline";
-        showLessBtn.style.display = "none";
-      });
-    } else {
-      container.innerHTML = truncatedText + "...";
-    }
-  } else {
-    const showMoreBtn = element.querySelector(".show-more");
-    const showLessBtn = element.querySelector(".show-less");
-    if (showMoreBtn && showLessBtn) {
-      showMoreBtn.style.display = "none";
-      showLessBtn.style.display = "none";
-    }
-  }
-};
-
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        if (entry.target.dataset.truncated) {
-          return;
+        const container = entry.target.querySelector(
+          "p, h2, h3, h4, h5, h6, div, span"
+        );
+        if (!container) return;
+
+        const maxLines =
+          window.innerWidth < 768
+            ? parseInt(entry.target.getAttribute("data-max-lines-mobile")) ||
+              parseInt(entry.target.getAttribute("data-max-lines")) ||
+              Infinity
+            : window.innerWidth < 1024
+            ? parseInt(entry.target.getAttribute("data-max-lines-tablet")) ||
+              parseInt(entry.target.getAttribute("data-max-lines")) ||
+              Infinity
+            : parseInt(entry.target.getAttribute("data-max-lines-desktop")) ||
+              parseInt(entry.target.getAttribute("data-max-lines")) ||
+              Infinity;
+
+        const lineHeight = parseFloat(getComputedStyle(container).lineHeight);
+        const maxContainerHeight = lineHeight * maxLines;
+
+        container.style.height = "auto";
+        const fullContainerHeight = container.offsetHeight;
+        container.style.height = "";
+
+        if (fullContainerHeight > maxContainerHeight) {
+          let truncatedText = container.innerHTML;
+          while (
+            container.offsetHeight > maxContainerHeight &&
+            truncatedText.length > 0
+          ) {
+            truncatedText = truncatedText.slice(0, -1);
+            container.innerHTML = truncatedText + "...";
+          }
+
+          const showMoreBtn = entry.target.querySelector(".show-more");
+          const showLessBtn = entry.target.querySelector(".show-less");
+          const shouldShowButtons =
+            container.offsetHeight < fullContainerHeight;
+
+          if (showMoreBtn && showLessBtn) {
+            showMoreBtn.style.display = shouldShowButtons ? "inline" : "none";
+            showLessBtn.style.display = "none";
+
+            showMoreBtn.addEventListener("click", () => {
+              container.innerHTML = container.dataset.fullText;
+              showMoreBtn.style.display = "none";
+              showLessBtn.style.display = "inline";
+            });
+
+            showLessBtn.addEventListener("click", () => {
+              container.innerHTML = container.dataset.truncatedText;
+              showMoreBtn.style.display = "inline";
+              showLessBtn.style.display = "none";
+            });
+          } else {
+            container.dataset.truncatedText = truncatedText + "...";
+            container.innerHTML = container.dataset.truncatedText;
+          }
+        } else {
+          const showMoreBtn = entry.target.querySelector(".show-more");
+          const showLessBtn = entry.target.querySelector(".show-less");
+          if (showMoreBtn && showLessBtn) {
+            showMoreBtn.style.display = "none";
+            showLessBtn.style.display = "none";
+          }
         }
-        truncateText(entry.target);
+
         entry.target.dataset.truncated = true;
         observer.unobserve(entry.target);
       }
     });
   },
-  { root: null, threshold: 0.1 }
+  { root: null, threshold: 0 }
 );
 
-const observeElements = () => {
-  document.querySelectorAll("[data-max-lines]").forEach((element) => {
-    observer.observe(element);
-  });
-
-  window.setTimeout(() => {
-    observer.disconnect();
-    document.querySelectorAll("[data-max-lines]").forEach((element) => {
-      if (!element.dataset.truncated) {
-        truncateText(element);
-        element.dataset.truncated = true;
-      }
-    });
-  }, 500);
-};
-
-const debouncedObserveElements = debounce(observeElements, 50);
-
-window.addEventListener("load", debouncedObserveElements);
+document.querySelectorAll("[data-max-lines]").forEach((element) => {
+  observer.observe(element);
+});
