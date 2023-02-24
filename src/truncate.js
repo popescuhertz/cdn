8d133c725f5b198b035b3b71ab9ad929c00dc6d6
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 12:28:08 -0800

    V

--- src/truncate.js
+++ src/truncate.js
@@ -1,157 +1,116 @@
-(function ($) {
-  var lineHeight = 0,
-    crrntLine = 0,
-    crrntWord = 0,
-    crrntLineOffset = 0,
-    needSuffix = 0;
-
-  /*
-   * Line
-   * @param {Object} element
-   * @param {Number} lineNumber
-   * @param {String} endStr
-   */
-  $.fn.line = function (lineNumber, endStr) {
-    var targets = this;
-
-    targets.each(function (i, e) {
-      var element = $(e),
-        txt = element.text(),
-        editedLines = "";
-
-      // Check for Default Values
-      lineNumber = lineNumber ? lineNumber : 2;
-      endStr = endStr ? endStr : "...";
-
-      // Check if parent el has 'static' position
-      if (element.css("position") === "static") {
-        element.css("position", "relative");
-      }
-
-      // Create storage for initial text
-      if (!element.data("initial-content")) {
-        element.data("initial-content", txt);
-      }
-
-      // Check for initial text length
-      if (element.data("initial-content").length >= txt.length) {
-        txt = element.data("initial-content");
-      }
-
-      element.html(wrapWords(txt.split(" ")).join(""));
-
-      editedLines = wrapLines(
-        splitToLines(element.find("span"), lineNumber),
-        lineNumber
-      );
-
-      element.html(addSuffix(editedLines, endStr));
-
-      console.log("Finish!");
-
-      // Refresh
-      (lineHeight = 0), (crrntLine = 0), (crrntWord = 0), (crrntLineOffset = 0);
-    });
-  };
-
-  /*
-   * Split text content in to lines
-   * @param {Array} words
-   * @return {Array} arrLines
-   */
-  function splitToLines(words, lineNumber) {
-    var arrLines = [];
-
-    for (var i = 0; i < words.length; i++, crrntWord++) {
-      if (i === 0) {
-        arrLines[0] = [];
-
-        if (words.eq(i).position().top !== 0) {
-          crrntLineOffset = words.eq(i).position().top;
-        }
-      }
-
-      checkOffset = words.eq(i).position().top;
-      if (checkOffset > crrntLineOffset) {
-        crrntLineOffset = checkOffset;
-        crrntLine++;
-        crrntWord = 0;
-        arrLines[crrntLine] = [];
-      }
-      arrLines[crrntLine][crrntWord] = words.eq(i);
-    }
-
-    // Check if suffix are needed
-    if (arrLines[lineNumber]) {
-      needSuffix = 1;
-    }
-
-    return arrLines;
+class CuttrBreakpoints {
+  constructor(selector, options = {}, breakpoints = []) {
+    this.selector = selector;
+    this.options = { ...defaults, ...options };
+    this.breakpoints = breakpoints;
+    this._init();
   }
 
-  /*
-   * Wrap Words to spans
-   * @private
-   * @return {Array} arrWords
-   */
-  function wrapWords(words) {
-    arrWords = [];
+  _init() {
+    const el = document.querySelectorAll(this.selector);
 
-    for (var i = words.length; i--; ) {
-      var crrntWord = words[i].trim();
+    if (el.length > 0) {
+      el.forEach((element) => {
+        let options = { ...this.options };
+        const elementBreakpoints = this.breakpoints.filter(
+          (bp) => window.matchMedia(bp.query).matches
+        );
+        elementBreakpoints.forEach(
+          (bp) => (options = { ...options, ...bp.options })
+        );
 
-      arrWords[i] = "<span>" + crrntWord + "</span> ";
+        new Cuttr(element, options);
+      });
     }
-
-    return arrWords;
   }
+}
 
-  /*
-   * Wrap Lines to spans
-   * @private
-   * @param {Array|String} lines
-   * @return {String} linesString
-   */
-  function wrapLines(lines, linesNumber) {
-    linesString = "";
-    for (var i = 0; i < linesNumber; i++) {
-      var line = "";
+const defaults = {
+  licenseKey: "2E864F64-86BB4151-AD9A08AF-B0B5C5BA",
+  truncate: "words",
+  length: 100,
+  ending: "...",
+  loadedClass: "cuttr--loaded",
+  title: false,
+  readMore: true,
+  readMoreText: "Read more",
+  readLessText: "Read less",
+  readMoreBtnPosition: "inside",
+  readMoreBtnTag: "button",
+  readMoreBtnSelectorClass: "read-more",
+  readMoreBtnAdditionalClasses: "",
+};
 
-      // Check if line amount aren't to low
-      if (lines[i]) {
-        lineLength = lines[i].length;
-      } else {
-        break;
-      }
+const cuttrClasses = [
+  {
+    selector: ".product-description.is-header",
+    options: {
+      length: 40,
+      readMoreBtnPosition: "after",
+    },
+    breakpoints: [
+      {
+        query: "(max-width: 480px)",
+        options: {
+          length: 20,
+        },
+      },
+      {
+        query: "(min-width: 481px) and (max-width: 768px)",
+        options: {
+          length: 30,
+        },
+      },
+    ],
+  },
+  {
+    selector: ".product-description.is-section",
+    options: {
+      length: 60,
+    },
+    breakpoints: [
+      {
+        query: "(max-width: 480px)",
+        options: {
+          length: 40,
+        },
+      },
+      {
+        query: "(min-width: 481px) and (max-width: 768px)",
+        options: {
+          length: 50,
+        },
+      },
+    ],
+  },
+  {
+    selector: ".product-features.is-section",
+    options: {
+      length: 40,
+    },
+    breakpoints: [
+      {
+        query: "(max-width: 480px)",
+        options: {
+          length: 30,
+        },
+      },
+      {
+        query: "(min-width: 481px) and (max-width: 768px)",
+        options: {
+          length: 40,
+        },
+      },
+    ],
+  },
+];
 
-      for (var j = 0; j < lineLength; j++) {
-        line += lines[i][j].text() + " ";
-      }
+Cuttr.prototype.defaults = defaults;
 
-      linesString += line + "\n";
-    }
+const cuttrInstances = [];
 
-    return linesString.trim();
-  }
-
-  /*
-   * Add suffix
-   * @private
-   * @param {String} str
-   * @param {String} suffix
-   */
-  function addSuffix(str, suffix) {
-    if (needSuffix) {
-      suffixLen = suffix.length;
-      str = str.slice(0, -suffixLen);
-      str += suffix;
-
-      // Remove Space before Suffix
-      str = str.replace(" " + suffix, suffix);
-
-      needSuffix = 0;
-    }
-
-    return str;
-  }
-})(jQuery);
+for (const classObj of cuttrClasses) {
+  const { selector, options, breakpoints } = classObj;
+  const cuttrInstance = new CuttrBreakpoints(selector, options, breakpoints);
+  cuttrInstances.push(cuttrInstance);
+}
