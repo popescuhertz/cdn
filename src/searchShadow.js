359fefac3678f85b0fa3bc86fa749375c8a025d1
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 14:47:46 -0800

    W

--- src/searchShadow.js
+++ src/searchShadow.js
@@ -1,6 +1,10 @@
-// Add shadow to search bar when input is focused
 const searchBarInput = document.querySelector(".search-input");
 const searchBar = document.querySelector(".search");
+
 searchBarInput.addEventListener("focus", () => {
-  searchBar.style.boxShadow = "0 0 1px 1px #191919";
+  searchBar.style.boxShadow = "0 0 1px 1px #f2f2f2";
+});
+
+searchBarInput.addEventListener("blur", () => {
+  searchBar.style.boxShadow = "none";
 });
