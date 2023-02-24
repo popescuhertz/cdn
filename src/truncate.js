7dfc2d62c8e5001ec633dc287d454deed2f67edc
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-23 18:21:16 -0800

    S

--- src/truncate.js
+++ src/truncate.js
@@ -1,87 +1,63 @@
-new Cuttr(".global-truncate", {
+// Define global options
+Cuttr.setGlobalOptions({
   licenseKey: "2E864F64-86BB4151-AD9A08AF-B0B5C5BA",
 });
 
-$(document).ready(function () {
-  // Define an object that maps each class to its truncation options
-  const classOptions = {
-    ".element1": {
+// Define options for each class and breakpoint
+const options = [
+  {
+    class: ".truncate-1",
+    breakpoints: [
+      { width: 768, options: { truncate: "words", length: 20 } },
+      { width: 992, options: { truncate: "words", length: 30 } },
+      { width: 1200, options: { truncate: "words", length: 40 } },
+    ],
+    defaultOptions: {
       truncate: "words",
-      length: 20,
+      length: 10,
     },
-    ".element2": {
-      truncate: "words",
-      length: 30,
-      readMore: true,
-    },
-    ".element3": {
+  },
+  {
+    class: ".truncate-2",
+    breakpoints: [
+      {
+        width: 768,
+        options: {
+          truncate: "characters",
+          length: 100,
+        },
+      },
+      {
+        width: 992,
+        options: {
+          truncate: "characters",
+          length: 150,
+        },
+      },
+      {
+        width: 1200,
+        options: {
+          truncate: "characters",
+          length: 200,
+        },
+      },
+    ],
+    defaultOptions: {
       truncate: "characters",
-      length: 100,
+      length: 50,
     },
-  };
+  },
+];
 
-  // Define an object that maps each breakpoint to the corresponding class options
-  const breakpointOptions = {
-    576: {
-      // Breakpoint for screens smaller than 576px
-      ".element1": {
-        length: 10,
-      },
-      ".element2": {
-        length: 15,
-      },
-      ".element3": {
-        length: 50,
-      },
-    },
-    992: {
-      // Breakpoint for screens smaller than 992px
-      ".element1": {
-        length: 15,
-      },
-      ".element2": {
-        length: 25,
-      },
-      ".element3": {
-        length: 80,
-      },
-    },
-    1200: {
-      // Breakpoint for screens larger than 1200px
-      ".element1": {
-        length: 20,
-      },
-      ".element2": {
-        length: 30,
-      },
-      ".element3": {
-        length: 100,
-      },
-    },
-  };
+// Initialize Cuttr for each class and breakpoint
+options.forEach((option) => {
+  const { class: className, breakpoints, defaultOptions } = option;
 
-  // Function to apply the appropriate options to each element based on the current viewport width
-  function applyOptions() {
-    const viewportWidth = $(window).width();
-    let options = {};
+  const instance = new Cuttr(className, defaultOptions);
 
-    // Iterate through each class and merge the default options with the breakpoint-specific options
-    Object.keys(classOptions).forEach((className) => {
-      options[className] = {
-        ...classOptions[className],
-        ...breakpointOptions[viewportWidth]?.[className],
-      };
-    });
+  breakpoints.forEach((breakpoint) => {
+    const { width, options } = breakpoint;
 
-    // Initialize Cuttr for each element with the appropriate options
-    Object.keys(options).forEach((className) => {
-      $(className).Cuttr(options[className]);
-    });
-  }
-
-  // Initialize Cuttr with the default options for each class
-  applyOptions();
-
-  // Re-apply the appropriate options each time the viewport width changes
-  $(window).resize(applyOptions);
+    instance.addBreakpoint(width, options);
+  });
 });
