f5473b671f9451adc1128cfb3b9887c42e7eb691
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-23 17:33:35 -0800

    Y

--- src/truncate.js
+++ src/truncate.js
@@ -1,51 +1,51 @@
-// Define the breakpoints and their options
-const breakpointOptions = {
-  ".class1": [
-    { breakpoint: 480, options: { truncate: "words", length: 10 } },
-    { breakpoint: 768, options: { truncate: "words", length: 20 } },
-    { breakpoint: 1024, options: { truncate: "words", length: 30 } },
-  ],
-  ".class2": [
-    { breakpoint: 480, options: { truncate: "words", length: 5 } },
-    { breakpoint: 768, options: { truncate: "words", length: 10 } },
-    { breakpoint: 1024, options: { truncate: "words", length: 15 } },
-  ],
-};
+$(document).ready(function () {
+  // Default options for all elements
+  var defaultOptions = {
+    truncate: "words",
+    length: 12,
+  };
 
-// Loop through each breakpointOptions key-value pair and create Cuttr instances for each breakpoint
-Object.entries(breakpointOptions).forEach(([selector, optionsArray]) => {
-  const element = document.querySelector(selector);
-  if (element) {
-    const cuttrInstances = optionsArray.map(({ breakpoint, options }) => {
-      return window.innerWidth < breakpoint
-        ? new Cuttr(element, options)
-        : null;
+  // Truncation options for different breakpoints and classes
+  var options = [
+    {
+      class: ".element-a",
+      breakpoints: [
+        { width: 768, options: { truncate: "words", length: 8 } },
+        { width: 992, options: { truncate: "characters", length: 50 } },
+      ],
+    },
+    {
+      class: ".element-b",
+      breakpoints: [
+        {
+          width: 768,
+          options: {
+            truncate: "words",
+            length: 6,
+          },
+        },
+        {
+          width: 992,
+          options: {
+            truncate: "characters",
+            length: 30,
+          },
+        },
+      ],
+    },
+  ];
+
+  // Loop through each set of options
+  options.forEach(function (option) {
+    // Loop through each breakpoint for this class
+    option.breakpoints.forEach(function (breakpoint) {
+      // Apply the options for this breakpoint to the element
+      var optionsForBreakpoint = $.extend(
+        {},
+        defaultOptions,
+        breakpoint.options
+      );
+      $(option.class).responsiveCuttr(optionsForBreakpoint, breakpoint.width);
     });
-    // Store the Cuttr instances in the element's dataset for later reference
-    element.dataset.cuttrInstances = JSON.stringify(cuttrInstances);
-  }
-});
-
-// Add a resize event listener to update the Cuttr instances when the window is resized
-window.addEventListener("resize", () => {
-  Object.entries(breakpointOptions).forEach(([selector, optionsArray]) => {
-    const element = document.querySelector(selector);
-    if (element) {
-      const cuttrInstances = JSON.parse(element.dataset.cuttrInstances);
-      cuttrInstances.forEach((instance, i) => {
-        if (instance) {
-          const { breakpoint, options } = optionsArray[i];
-          if (window.innerWidth < breakpoint) {
-            if (!instance.isInitialized()) {
-              instance.initialize(options);
-            }
-          } else {
-            if (instance.isInitialized()) {
-              instance.destroy();
-            }
-          }
-        }
-      });
-    }
   });
 });
