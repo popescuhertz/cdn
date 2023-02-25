408c7fc2eb448a7f13c4672c6358aecc174a5000
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 16:59:37 -0800

    Q

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,9 +1,16 @@
 // select the iframe element
 const iframe = document.querySelector(".section iframe");
-console.log(iframe);
 
-// check the value of the data-embed-state attribute
-const embedState = iframe.getAttribute("data-embed-state");
+// get a reference to the iframe's window object
+const iframeWindow = iframe.contentWindow;
+
+// get a reference to the iframe's document object
+const iframeDocument = iframeWindow.document;
+
+// select the element inside the iframe and get its attribute
+const embedState = iframeDocument
+  .querySelector("element-selector")
+  .getAttribute("data-embed-state");
 
 // hide the iframe if data-embed-state is "error"
 if (embedState === "error") {
