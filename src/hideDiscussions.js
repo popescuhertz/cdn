feb38ef901cc3ae45eadbe5bccf6997099d49c07
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 17:07:54 -0800

    R

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -4,21 +4,26 @@ const section = document.querySelector(".section");
 // select the iframe element
 const iframe = section.querySelector("iframe");
 
-// wait for the iframe to load its content
-iframe.addEventListener("load", function () {
-  // get a reference to the iframe's window object
-  const iframeWindow = iframe.contentWindow;
+// check if the iframe element is found
+if (iframe) {
+  // wait for the iframe to load its content
+  iframe.addEventListener("load", function () {
+    // get a reference to the iframe's window object
+    const iframeWindow = iframe.contentWindow;
 
-  // get a reference to the iframe's document object
-  const iframeDocument = iframeWindow.document;
+    // get a reference to the iframe's document object
+    const iframeDocument = iframeWindow.document;
 
-  // select the element inside the iframe and get its attribute
-  const embedState = iframeDocument
-    .querySelector("element-selector")
-    .getAttribute("data-embed-state");
+    // select the element inside the iframe and get its attribute
+    const embedState = iframeDocument
+      .querySelector("element-selector")
+      .getAttribute("data-embed-state");
 
-  // hide the section if the iframe is empty or if data-embed-state is "error"
-  if (iframeDocument.body.innerHTML.trim() === "" || embedState === "error") {
-    section.style.display = "none";
-  }
-});
+    // hide the section if the iframe is empty or if data-embed-state is "error"
+    if (iframeDocument.body.innerHTML.trim() === "" || embedState === "error") {
+      section.style.display = "none";
+    }
+  });
+} else {
+  console.log("iframe not found");
+}
