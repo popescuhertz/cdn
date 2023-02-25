993ec6025d14dd5c4960596a8307363eca69d5a7
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 16:48:22 -0800

    W

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,20 +1,26 @@
-function hideSectionsWithError() {
-  const sections = document.querySelectorAll(".section");
+// Get all sections with class "section"
+const sections = document.querySelectorAll(".section");
 
-  sections.forEach((section) => {
-    const discussions = section.querySelectorAll(".discussions");
-    const header = section.querySelector(".discussion-embed header");
-    const iframe = header ? header.querySelector("iframe") : null;
-
-    if (
-      discussions.length > 0 &&
-      iframe &&
-      iframe.contentDocument.querySelector("html > body > header").dataset
-        .embedState === "error"
-    ) {
+// Loop through each section
+sections.forEach((section) => {
+  // Check if the section has a child element matching the specified selector
+  const iframe = section.querySelector(
+    ".product-section > .discussions > .discussion-embed > #discourse-comments > iframe"
+  );
+  if (
+    iframe &&
+    iframe.contentDocument &&
+    iframe.contentDocument.documentElement
+  ) {
+    const header =
+      iframe.contentDocument.documentElement.querySelector("header");
+    // Check if the header has data-embed-state="error"
+    if (header && header.getAttribute("data-embed-state") === "error") {
+      // Hide the section
       section.style.display = "none";
     } else {
+      // Show the section
       section.style.display = "block";
     }
-  });
-}
+  }
+});
