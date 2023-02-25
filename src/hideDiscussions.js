3d75e5bcea500ad49b1decbe2d8588ba19b71606
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 16:50:57 -0800

    E

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,9 +1,6 @@
-// Get all sections with class "section"
 const sections = document.querySelectorAll(".section");
 
-// Loop through each section
 sections.forEach((section) => {
-  // Check if the section has a child element matching the specified selector
   const iframe = section.querySelector(
     ".product-section > .discussions > .discussion-embed > #discourse-comments > iframe"
   );
@@ -14,13 +11,16 @@ sections.forEach((section) => {
   ) {
     const header =
       iframe.contentDocument.documentElement.querySelector("header");
-    // Check if the header has data-embed-state="error"
-    if (header && header.getAttribute("data-embed-state") === "error") {
-      // Hide the section
-      section.style.display = "none";
-    } else {
-      // Show the section
-      section.style.display = "block";
+    if (header) {
+      const embedState = header.getAttribute("data-embed-state");
+      console.log(`Embed state for section ${section.id}: ${embedState}`);
+      if (embedState === "error") {
+        section.style.display = "none";
+        console.log(`Hiding section ${section.id}`);
+      } else {
+        section.style.display = "block";
+        console.log(`Showing section ${section.id}`);
+      }
     }
   }
 });
