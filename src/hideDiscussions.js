83c12b16e786f82b081bf29b25793aae7caab681
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 16:27:40 -0800

    Y

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,3 +1,18 @@
+// Find all elements with class "section"
+var sections = document.querySelectorAll(".section");
+
+// Listen for errors on the "embed.js" script
+var embedScript = document.querySelector('script[src*="embed.js"]');
+embedScript.addEventListener("error", function () {
+  // If an error occurs, log a message to the console and hide sections with .discussions
+  console.error("Failed to load resource: net::ERR_NAME_NOT_RESOLVED");
+  sections.forEach(function (section) {
+    if (section.querySelector(".discussions")) {
+      section.style.display = "none";
+    }
+  });
+});
+
 // Wait for the page to finish loading
 window.addEventListener("load", function () {
   // Find all elements with class "section"
@@ -13,20 +28,6 @@ window.addEventListener("load", function () {
       var shouldHide = true;
     }
   }
-
-  // Listen for errors on the "embed.js" script
-  var embedScript = document.querySelector('script[src*="embed.js"]');
-  embedScript.addEventListener("error", function () {
-    // If an error occurs, log a message to the console and hide sections with .discussions
-    console.error("Failed to load resource: net::ERR_NAME_NOT_RESOLVED");
-    if (shouldHide) {
-      sections.forEach(function (section) {
-        if (section.querySelector(".discussions")) {
-          section.style.display = "none";
-        }
-      });
-    }
-  });
 
   embedScript.addEventListener("load", function () {
     // If the script loads successfully, log a message to the console and unhide sections with .discussions
