bec4861b447a620f8a6505033f25812551a59177
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 16:38:25 -0800

    Q

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,33 +1,18 @@
-window.addEventListener("load", function () {
-  var sections = document.querySelectorAll(".section");
-  for (var i = 0; i < sections.length; i++) {
-    var section = sections[i];
-    if (section.querySelector(".discussions")) {
-      section.style.display = "none";
-    }
-  }
-});
+function hideSectionsWithError() {
+  const sections = document.querySelectorAll(".section");
 
-var embedJS = document.querySelector('script[src*="embed.js"]');
-if (embedJS) {
-  embedJS.addEventListener("error", function () {
-    console.error("Failed to load resource: net::ERR_NAME_NOT_RESOLVED");
-    var sections = document.querySelectorAll(".section");
-    for (var i = 0; i < sections.length; i++) {
-      var section = sections[i];
-      if (section.querySelector(".discussions")) {
-        section.style.display = "none";
-      }
-    }
-  });
-} else {
-  var sections = document.querySelectorAll(".section");
-  for (var i = 0; i < sections.length; i++) {
-    var section = sections[i];
-    if (section.querySelector(".discussions")) {
+  sections.forEach((section) => {
+    const discussions = section.querySelectorAll(".discussions");
+    const iframe = section.querySelector("iframe");
+
+    if (
+      discussions.length > 0 &&
+      iframe &&
+      iframe.dataset.embedState === "error"
+    ) {
       section.style.display = "none";
     } else {
-      section.style.display = "block"; // or whatever default display value you want to use
+      section.style.display = "block";
     }
-  }
+  });
 }
