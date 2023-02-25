a6c46aa40fbc292aa2fd9ca433cfc189fa348383
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 17:01:17 -0800

    W

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,18 +1,21 @@
 // select the iframe element
 const iframe = document.querySelector(".section iframe");
 
-// get a reference to the iframe's window object
-const iframeWindow = iframe.contentWindow;
+// wait for the iframe to load its content
+iframe.addEventListener("load", function () {
+  // get a reference to the iframe's window object
+  const iframeWindow = iframe.contentWindow;
 
-// get a reference to the iframe's document object
-const iframeDocument = iframeWindow.document;
+  // get a reference to the iframe's document object
+  const iframeDocument = iframeWindow.document;
 
-// select the element inside the iframe and get its attribute
-const embedState = iframeDocument
-  .querySelector("element-selector")
-  .getAttribute("data-embed-state");
+  // select the element inside the iframe and get its attribute
+  const embedState = iframeDocument
+    .querySelector("element-selector")
+    .getAttribute("data-embed-state");
 
-// hide the iframe if data-embed-state is "error"
-if (embedState === "error") {
-  iframe.style.display = "none";
-}
+  // hide the iframe if data-embed-state is "error"
+  if (embedState === "error") {
+    iframe.style.display = "none";
+  }
+});
