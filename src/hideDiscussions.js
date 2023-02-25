497f608c9cc1baaede68036571a2fd8f2d3bc83d
popescuhertz <33972146+popescuhertz@users.noreply.github.com>
2023-02-24 16:31:21 -0800

    U

--- src/hideDiscussions.js
+++ src/hideDiscussions.js
@@ -1,43 +1,33 @@
-// Find all elements with class "section"
-var sections = document.querySelectorAll(".section");
-
-// Listen for errors on the "embed.js" script
-var embedScript = document.querySelector('script[src*="embed.js"]');
-embedScript.addEventListener("error", function () {
-  // If an error occurs, log a message to the console and hide sections with .discussions
-  console.error("Failed to load resource: net::ERR_NAME_NOT_RESOLVED");
-  sections.forEach(function (section) {
+window.addEventListener("load", function () {
+  var sections = document.querySelectorAll(".section");
+  for (var i = 0; i < sections.length; i++) {
+    var section = sections[i];
     if (section.querySelector(".discussions")) {
       section.style.display = "none";
     }
-  });
+  }
 });
 
-// Wait for the page to finish loading
-window.addEventListener("load", function () {
-  // Find all elements with class "section"
+var embedJS = document.querySelector('script[src*="embed.js"]');
+if (embedJS) {
+  embedJS.addEventListener("error", function () {
+    console.error("Failed to load resource: net::ERR_NAME_NOT_RESOLVED");
+    var sections = document.querySelectorAll(".section");
+    for (var i = 0; i < sections.length; i++) {
+      var section = sections[i];
+      if (section.querySelector(".discussions")) {
+        section.style.display = "none";
+      }
+    }
+  });
+} else {
   var sections = document.querySelectorAll(".section");
-
-  // Loop over the sections
   for (var i = 0; i < sections.length; i++) {
     var section = sections[i];
-
-    // Check if the section has a descendant with class "discussions"
     if (section.querySelector(".discussions")) {
-      // If it does, set a flag indicating that it should be hidden
-      var shouldHide = true;
+      section.style.display = "none";
+    } else {
+      section.style.display = "block"; // or whatever default display value you want to use
     }
   }
-
-  embedScript.addEventListener("load", function () {
-    // If the script loads successfully, log a message to the console and unhide sections with .discussions
-    console.log("Embed script loaded successfully");
-    if (shouldHide) {
-      sections.forEach(function (section) {
-        if (section.querySelector(".discussions")) {
-          section.style.display = "";
-        }
-      });
-    }
-  });
-});
+}
